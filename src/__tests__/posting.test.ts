a633ed9713af9ff2b1d3591a6a2e84e4f33eab7f
Pierre Criulanscy <pierre_criulanscy.external@discovery.com>
2021-07-23 20:38:18 +0200

    refactor(posting): refactor tests

--- src/__tests__/posting.test.ts
+++ src/__tests__/posting.test.ts
@@ -2,54 +2,75 @@ import { createInMemoryTimelineRepository } from '../adapters/repositories';
 import { Timeline, User } from '../domain';
 import { createPostMessage } from '../use-cases';
 
+const buildTestTimeline = ({ userName, messages }: { userName: string, messages: string[] }) => {
+  const user = User.create({ name: userName });
+  const timeline = Timeline.of({ user, messages });
+
+  return timeline;
+}
+
+const givenUserWantingToPostNewMessage = ({ userName, existingMessages }: { userName: string, existingMessages: string[] }) => {
+  const timeline = buildTestTimeline({ userName, messages: existingMessages });
+  const timelineRepository = createInMemoryTimelineRepository({ initialTimeline: timeline });
+  const postMessage = createPostMessage({ timelineRepository });
+
+  return {
+    user: timeline.getUser(),
+    postMessage: ({ message }: { message: string }) => postMessage({ user: userName, message }),
+    getEditedTimeline() {
+      return timelineRepository.getForUser({ user: userName });
+    }
+  }
+}
+
+
 describe('posting a message to a personnal timeline', () => {
 
   it('should post a message to the personnal empty timeline', async () => {
     // arrange
-    const timelineRepository = createInMemoryTimelineRepository();
-    const alice = User.create({ name: 'Alice' });
-    const message = 'Coucou le tchat twitch';
-    const postMessage = createPostMessage({ timelineRepository });
+    const { postMessage, getEditedTimeline, user: Alice } = givenUserWantingToPostNewMessage({
+      userName: 'Alice',
+      existingMessages: []
+    });
     
     // act
-    await postMessage({ user: alice.getName(), message });
+    await postMessage({ message: 'Coucou le tchat twitch' });
 
     // assert
-    const timeline = await timelineRepository.getForUser({ user: alice.getName() });
-    expect(timeline).toEqual(Timeline.of({ user: alice, messages: ['Coucou le tchat twitch'] }));
+    const editedTimeline = await getEditedTimeline();
+    expect(editedTimeline).toEqual(Timeline.of({ user: Alice, messages: ['Coucou le tchat twitch'] }));
   });
 
   it('should post a message with a different user to her\'s empty timeline', async () => {
     // arrange
-    const timelineRepository = createInMemoryTimelineRepository();
-    const bob = User.create({ name: 'Bob' });
-    const message = 'Coucou c\'est Bob';
-    const postMessage = createPostMessage({ timelineRepository });
+    const { postMessage, getEditedTimeline, user: Bob } = givenUserWantingToPostNewMessage({
+      userName: 'Bob',
+      existingMessages: []
+    });
     
     // act
-    await postMessage({ user: bob.getName(), message });
+    await postMessage({ message: 'Coucou c\'est Bob' });
 
     // assert
-    const timeline = await timelineRepository.getForUser({ user: bob.getName() });
-    expect(timeline).toEqual(Timeline.of({ user: bob, messages: [
-      'Coucou c\'est Bob',
-    ]}));
+    const editedTimeline = await getEditedTimeline();
+    expect(editedTimeline).toEqual(Timeline.of({ user: Bob, messages: ['Coucou c\'est Bob'] }));
   });
 
   it('alice should post a message in her\'s non empty timeline', async () => {
     // arrange
-    const alice = User.create({ name: 'alice' });
-    const timeline = Timeline.of({ user: alice, messages: ['hello world'] })
-    const timelineRepository = createInMemoryTimelineRepository({ initialTimeline: timeline });
-    const postMessage = createPostMessage({ timelineRepository });
-
-    // act
-    await postMessage({ user: alice.getName(), message: 'comment ça va ?'});
+    const { postMessage, getEditedTimeline, user: Alice } = givenUserWantingToPostNewMessage({
+      userName: 'Alice',
+      existingMessages: ['hello world']
+    });
     
+    // act
+    await postMessage({ message: 'comment ça va ?' });
+
     // assert
-    expect(timeline).toEqual(Timeline.of({ user: alice, messages: [
+    const editedTimeline = await getEditedTimeline();
+    expect(editedTimeline).toEqual(Timeline.of({ user: Alice, messages: [
       'hello world',
       'comment ça va ?'
-    ]}));
+    ] }));
   })
 });
